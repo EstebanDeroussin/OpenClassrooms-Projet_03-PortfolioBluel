5a2b9fc02f20c6fbca4bae3f2ddbfb94ab7e723e
Esteban DEROUSSIN <esteban.deroussin@icloud.com>
2025-12-14 16:55:22 +0100

    ajout de l'afficheage/suppression des differents éléments du dom selon l'authentificatiion de l'utilisateur

--- FrontEnd/src/views/auth-view.mjs
+++ FrontEnd/src/views/auth-view.mjs
@@ -2,9 +2,13 @@
 export function showAdminFeatures() {
 	const bandeau = document.querySelector(".bandeau");
 	const header = document.querySelector(".header-content");
-	const edit_btn = document.querySelector(".edit-works");
-    const logout_btn = document.querySelector(".logout");
-    
+	const edit_btn = document.querySelector(".edit-btn");
+	const logout_btn = document.querySelector(".logout");
+	const login_btn = document.querySelector(".login-btn");
+	const title = document.querySelector(".my-project");
+
+	title.classList.remove("active");
+	login_btn.classList.remove("active");
 	bandeau.classList.add("active");
 	header.classList.add("active");
 	edit_btn.classList.add("active");
@@ -14,11 +18,15 @@ export function showAdminFeatures() {
 export function hideAdminFeatures() {
 	const bandeau = document.querySelector(".bandeau");
 	const header = document.querySelector(".header-content");
-    const edit_btn = document.querySelector(".edit-works");
-    const logout_btn = document.querySelector(".logout");
-    
+	const edit_btn = document.querySelector(".edit-btn");
+	const logout_btn = document.querySelector(".logout");
+	const login_btn = document.querySelector(".login-btn");
+	const title = document.querySelector(".my-project");
+
+	title.classList.add("active");
+	login_btn.classList.add("active");
 	edit_btn.classList.remove("active");
 	bandeau.classList.remove("active");
-    header.classList.remove("active");
-    logout_btn.classList.remove("active")
+	header.classList.remove("active");
+	logout_btn.classList.remove("active");
 }
